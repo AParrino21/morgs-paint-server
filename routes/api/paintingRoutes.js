9eb8319fb9b65f2c85be29708de7c055817da207
Anthony Parrino <anthonyparrino@Anthonys-MBP.attlocal.net>
2023-10-18 21:36:41 -0400

    update

--- routes/api/paintingRoutes.js
+++ routes/api/paintingRoutes.js
@@ -1,144 +1,177 @@
 const router = require("express").Router();
 const db = require("../../models");
-require('dotenv').config();
+require("dotenv").config();
 
-const API = process.env.MORGS_API_URL
-const S = process.env.MORGS_S_URL
+const API = process.env.MORGS_API_URL;
+const S = process.env.MORGS_S_URL;
 
-router.get('/getCakeGirls', (req, res) => {
+router.get("/getCakeGirls", (req, res) => {
   fetch(`${API}cakeGirls`, {
-    method: 'GET',
+    method: "GET",
     headers: {
-    'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
-  }).then(response => {
-    return response.json()
-  }).then(data => {
-    return res.json(data)
   })
-})
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      return res.json(data);
+    });
+});
 
-router.get('/getHeader', (req, res) => {
+router.get("/getHeader", (req, res) => {
   fetch(`${API}headers`, {
-    method: 'GET',
+    method: "GET",
     headers: {
-    'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
-  }).then(response => {
-    return response.json()
-  }).then(data => {
-    return res.json(data)
   })
-})
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      return res.json(data);
+    });
+});
 
-router.get('/getContactPic', (req, res) => {
+router.get("/getContactPic", (req, res) => {
   fetch(`${API}contactPic`, {
-    method: 'GET',
+    method: "GET",
     headers: {
-    'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
-  }).then(response => {
-    return response.json()
-  }).then(data => {
-    return res.json(data)
   })
-})
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      return res.json(data);
+    });
+});
 
-router.get('/getGalleryHeader', (req, res) => {
+router.get("/getGalleryHeader", (req, res) => {
   fetch(`${API}galleryHeader`, {
-    method: 'GET',
+    method: "GET",
     headers: {
-    'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
-  }).then(response => {
-    return response.json()
-  }).then(data => {
-    return res.json(data)
   })
-})
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      return res.json(data);
+    });
+});
 
-router.get('/oils', (req, res) => {
+router.get("/oils", (req, res) => {
   db.Paintings.find({})
-    .then(dbModel => res.json(dbModel))
-    .catch(err => {
-      console.log(err)
-      res.status(422).json(err)
+    .then((dbModel) => res.json(dbModel))
+    .catch((err) => {
+      console.log(err);
+      res.status(422).json(err);
     });
-})
+});
 
-router.get('/oils/:id', (req, res) => {
+router.get("/oils/:id", (req, res) => {
   db.Paintings.findOne({ _id: req.params.id })
-    .then(dbModel => res.json(dbModel))
-    .catch(err => {
-      console.log(err)
-      res.status(422).json(err)
+    .then((dbModel) => res.json(dbModel))
+    .catch((err) => {
+      console.log(err);
+      res.status(422).json(err);
     });
-})
+});
 
-router.get('/mixedmedia', (req, res) => {
+router.get("/mixedmedia", (req, res) => {
   db.MixedMedia.find({})
-    .then(dbModel => res.json(dbModel))
-    .catch(err => {
-      console.log(err)
-      res.status(422).json(err)
+    .then((dbModel) => res.json(dbModel))
+    .catch((err) => {
+      console.log(err);
+      res.status(422).json(err);
     });
-})
+});
 
-router.get('/prints', (req, res) => {
+router.get("/prints", (req, res) => {
   db.Prints.find({})
-    .then(dbModel => res.json(dbModel))
-    .catch(err => {
-      console.log(err)
-      res.status(422).json(err)
+    .then((dbModel) => res.json(dbModel))
+    .catch((err) => {
+      console.log(err);
+      res.status(422).json(err);
     });
-})
+});
 
-router.get('/prints/:id', (req, res) => {
+router.get("/prints/:id", (req, res) => {
   db.Prints.findOne({ _id: req.params.id })
-    .then(dbModel => res.json(dbModel))
-    .catch(err => {
-      console.log(err)
-      res.status(422).json(err)
+    .then((dbModel) => res.json(dbModel))
+    .catch((err) => {
+      console.log(err);
+      res.status(422).json(err);
     });
-})
+});
 
-router.get('/mixedmedia/:id', (req, res) => {
+router.get("/mixedmedia/:id", (req, res) => {
   db.MixedMedia.findOne({ _id: req.params.id })
-    .then(dbModel => res.json(dbModel))
-    .catch(err => {
-      console.log(err)
-      res.status(422).json(err)
+    .then((dbModel) => res.json(dbModel))
+    .catch((err) => {
+      console.log(err);
+      res.status(422).json(err);
     });
-})
+});
 
-router.put('/subtract', async (req, res) => {
-  let data = req.body
-  if (data.cat === 'painting') {
-    db.Paintings.findByIdAndUpdate({ _id: data._id }, { inventory: data.inventory - 1 })
-      .then(dbModel => res.json(dbModel))
-      .catch(err => {
-        console.log(err)
-        res.status(422).json(err)
+router.put("/subtract", async (req, res) => {
+  let data = req.body;
+  if (data.cat === "painting") {
+    db.Paintings.findByIdAndUpdate(
+      { _id: data._id },
+      { inventory: data.inventory - 1 }
+    )
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => {
+        console.log(err);
+        res.status(422).json(err);
       });
   }
 
-  if(data.cat === 'mixed_media') {
-    db.MixedMedia.findByIdAndUpdate({ _id: data._id }, { inventory: data.inventory - 1 })
-    .then(dbModel => res.json(dbModel))
-    .catch(err => {
-      console.log(err)
-      res.status(422).json(err)
-    });
+  if (data.cat === "mixed_media") {
+    db.MixedMedia.findByIdAndUpdate(
+      { _id: data._id },
+      { inventory: data.inventory - 1 }
+    )
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => {
+        console.log(err);
+        res.status(422).json(err);
+      });
   }
 
-  if(data.cat === 'prints') {
-    db.Prints.updateOne({_id: data.objId, src: {$elemMatch: {image_id: data.image_id}}} , {$set: {"src.$.inventory": data.srcUpdate[0].inventory}})
-    .then(dbModel => res.json(dbModel))
-    .catch(err => {
-      console.log(err)
-      res.status(422).json(err)
-    });
+  if (data.cat === "prints") {
+    db.Prints.updateOne(
+      { _id: data.objId, src: { $elemMatch: { image_id: data.image_id } } },
+      { $set: { "src.$.inventory": data.srcUpdate[0].inventory } }
+    )
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => {
+        console.log(err);
+        res.status(422).json(err);
+      });
   }
-})
+
+  if (data.cat === "bday") {
+    fetch(`${API}subtractInventory`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        return res.json(data);
+      });
+  }
+});
 
 module.exports = router;
